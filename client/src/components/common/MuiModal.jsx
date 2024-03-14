fd6f9397d99d2a84d2abe3d978c5dda3a7256bdc
Aliseth <charlotte.saidi@outlook.fr>
2024-03-14 16:08:21 +0100

    feat(listing): ajout d'une modal

---
+++ client/src/components/common/MuiModal.jsx
@@ -0,0 +1,122 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { styled, css } from '@mui/system';
+import { Modal as BaseModal } from '@mui/base/Modal';
+import Fade from '@mui/material/Fade';
+
+export default function MuiModal({content, open, setOpen}) {
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        slots={{ backdrop: StyledBackdrop }}
+      >
+        <Fade in={open}>
+          <ModalContent sx={style}>
+            {content}
+          </ModalContent>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
+
+const Backdrop = React.forwardRef((props, ref) => {
+  const { open, ...other } = props;
+  return (
+    <Fade in={open}>
+      <div ref={ref} {...other} />
+    </Fade>
+  );
+});
+
+Backdrop.propTypes = {
+  open: PropTypes.bool,
+};
+
+const blue = {
+  200: '#99CCFF',
+  300: '#66B2FF',
+  400: '#3399FF',
+  500: '#007FFF',
+  600: '#0072E5',
+  700: '#0066CC',
+};
+
+const grey = {
+  50: '#F3F6F9',
+  100: '#E5EAF2',
+  200: '#DAE2ED',
+  300: '#C7D0DD',
+  400: '#B0B8C4',
+  500: '#9DA8B7',
+  600: '#6B7A90',
+  700: '#434D5B',
+  800: '#303740',
+  900: '#1C2025',
+};
+
+const Modal = styled(BaseModal)`
+  position: fixed;
+  z-index: 1300;
+  inset: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledBackdrop = styled(Backdrop)`
+  z-index: -1;
+  position: fixed;
+  inset: 0;
+  background-color: rgb(0 0 0 / 0.5);
+  -webkit-tap-highlight-color: transparent;
+`;
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+};
+
+const ModalContent = styled('div')(
+  ({ theme }) => css`
+    font-family: 'IBM Plex Sans', sans-serif;
+    font-weight: 500;
+    text-align: start;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    overflow: hidden;
+    background-color: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+    border-radius: 8px;
+    border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
+    box-shadow: 0 4px 12px
+      ${theme.palette.mode === 'dark' ? 'rgb(0 0 0 / 0.5)' : 'rgb(0 0 0 / 0.2)'};
+    padding: 24px;
+    color: ${theme.palette.mode === 'dark' ? grey[50] : grey[900]};
+
+    & .modal-title {
+      margin: 0;
+      line-height: 1.5rem;
+      margin-bottom: 8px;
+    }
+
+    & .modal-description {
+      margin: 0;
+      line-height: 1.5rem;
+      font-weight: 400;
+      color: ${theme.palette.mode === 'dark' ? grey[400] : grey[800]};
+      margin-bottom: 4px;
+    }
+  `,
+);
--- client/src/components/posts/tableListing.jsx
+++ client/src/components/posts/tableListing.jsx
@@ -1,7 +1,19 @@
-import {Button, Paper, styled, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import {
+  Alert,
+  Button,
+  Paper,
+  styled,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow
+} from "@mui/material";
 import moment from "moment/moment";
 import {BsPencilSquare, BsFillTrashFill} from "react-icons/bs";
 import React from "react";
+import MuiModal from "../common/MuiModal";
 
 const DeleteButton = styled(Button)`
     cursor: default;
@@ -12,45 +24,54 @@ const DeleteButton = styled(Button)`
 `
 
 const TableListing = ({items, handleDelete}) => {
+  const [open, setOpen] = React.useState(false);
+  const openModal = () => setOpen(true);
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="left">Poste</TableCell>
-            <TableCell align="left">Description</TableCell>
-            <TableCell align="left">Date de début</TableCell>
-            <TableCell align="left">Date de fin</TableCell>
-            <TableCell align="left">Actions</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {items.map((item) => (
-            <TableRow
-              key={item.id}
-              sx={{
-                '&:last-child td, &:last-child th': { border: 0 }
-              }}
-            >
-              <TableCell component="th" scope="row">{item.post.name}</TableCell>
-              <TableCell align="left">{item.title}</TableCell>
-              <TableCell align="left">{moment(item.startDate).format('L')}</TableCell>
-              <TableCell align="left">{moment(item.endDate).format('L')}</TableCell>
-              <TableCell align="left">
-                <div>
-                  <Button>
-                    <BsPencilSquare color={"red"} />
-                  </Button>
-                  <DeleteButton>
-                    <BsFillTrashFill color={"green"} onClick={() => handleDelete(item.id)}/>
-                  </DeleteButton>
-                </div>
-              </TableCell>
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">Poste</TableCell>
+              <TableCell align="left">Description</TableCell>
+              <TableCell align="left">Date de début</TableCell>
+              <TableCell align="left">Date de fin</TableCell>
+              <TableCell align="left">Actions</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {items.map((item) => (
+              <TableRow
+                key={item.id}
+                sx={{
+                  '&:last-child td, &:last-child th': { border: 0 }
+                }}
+              >
+                <TableCell component="th" scope="row">{item.post.name}</TableCell>
+                <TableCell align="left">{item.title}</TableCell>
+                <TableCell align="left">{moment(item.startDate).format('L')}</TableCell>
+                <TableCell align="left">{moment(item.endDate).format('L')}</TableCell>
+                <TableCell align="left">
+                  <div>
+                    <Button>
+                      <BsPencilSquare color={"red"} onClick={openModal} />
+                    </Button>
+                    <DeleteButton>
+                      <BsFillTrashFill color={"green"} onClick={() => handleDelete(item.id)}/>
+                    </DeleteButton>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <MuiModal
+        open={open}
+        setOpen={setOpen}
+        content={<Alert severity="warning">Fonctionnalité à venir</Alert>}
+      />
+    </>
   )
 }
 
